856223191a301f0f8ef549de3a7676e0bbe0fed9
Lucas <lucashnutn@gmail.com>
2019-09-09 06:19:55 -0300

    Script4 completo, afinar detalle en cuanto *.sh extension ingresada por usuario

--- Script4.sh
+++ Script4.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 	
+
 	read -p 'Ingrese una directorio a buscar : ' directorio
 	read -p 'Ingrese extension de fichero a buscar: ' extension
+	
+	
+	
+	
 
 	 if [ -e $directorio ]; then
 		cd $directorio
-		lista=$(ls -R .)
-		touch archivos-.$extension-en-$directorio
-		ls *.$extension >> archivos-$extension>en-$directorio
+		
+		
+		find . -name $extension >> archivos-$extension-en-$directorio
+		
+
+
+
+
 	else
 		echo "::: El directorio $directorio no existe :::"
 	fi
