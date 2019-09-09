f54f1c6a23424490ce4506c0b827b290299b4db2
Lucas <lucashnutn@gmail.com>
2019-09-09 18:18:16 -0300

    Script3.sh completo

--- Script3.sh
+++ Script3.sh
@@ -3,10 +3,37 @@
 
 	read -p 'Ingrese Host : ... ' host
 	read -p 'Ingrese Intervalo : ...' intervalo
+	
 
-	ping -c 10 -i $intervalo $host >> ping-$host &
 
+	
+	
+	#ping -c 10 -i $intervalo $host >> ping-$host &
 
+	
+	#ping -c1 -i $intervalo $host &>/dev/null 
+
+function delay (){
+
+       ping -c1 -i $intervalo $host &>/dev/null 
+
+	if [ $? -eq 0 ]; then
+	
+	
+	echo -e $(date +%c) - $host ' Conexion Exitosa' >> archivo &
+	
+	else
+	
+	echo -e $(date +%c) - $host ' Fallo Conexion' >> archivo &
+	
+	fi
+
+	( sleep 5  ; delay)&
+
+}
+
+    (delay)&
+		
 
 
 
