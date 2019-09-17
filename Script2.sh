4f067bcdf69bd121e9c88f6e6808c98d2feb4bc9
lucianomaxz <53871604+lucianomaxz@users.noreply.github.com>
2019-09-17 19:39:08 +0000

    Update Script2.sh

--- Script2.sh
+++ Script2.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Frias,Lucas
+#Canteros,Luciano
+#Fecha: 17/10/2019
+
+#Descripcion: Script que muestra diferentes caracteristicas del sistema.
 
 
 		echo -e "Fecha del reporte"
@@ -8,24 +13,24 @@
 		echo -e "Version del equipo y kernel" 
 
  		#cat /proc/version
-		lsb_release -d
-		uname -rs
+		lsb_release -d # Muestra una descripción de la distribución instalada actualmente.
+		uname -rs # Muestra version del equipo y kernel
 
 		echo -e
 		echo -e "Version del S.O"
 		
-		uname -o
+		uname -o # Muestra la version del sistema operativo
 
 		
 		echo -e 'Hardware: Procesador' 
 		#sudo lshw -short -C cpu
-		grep -m 1 'model name' /proc/cpuinfo
-		echo -e
+		grep -m 1 'model name' /proc/cpuinfo # grep se utiliza para buscar lineas de textos coincedente con la ingresada, dentro del archivo /cpuinfo  
+		echo -e				     # -m Muestra un numero de concidencias con el texto ingresado y finaliza la busqueda
 		echo -e 'memoria'	
-		free -m
-		echo -e 'perifericos' 
-		lsusb
-		cat /proc/scsi/scsi
+		free -m				     # free Muestra la cantidad de memoria libre y usada en el sistema
+		echo -e 'perifericos' 		     # -m Muestra la memoria en mebibytes
+		lsusb				
+		cat /proc/scsi/scsi		     # cat Muestra la informacion de un archivo por consola	
 
 
 		echo -e 'Espacio de disco y particiones montadas'
