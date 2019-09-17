d784304e500afbaec613dee7bfdb8dcfc6302cd9
Lucas Frias <lucashnutn@gmail.com>
2019-09-17 17:28:35 -0300

    Update Script4.sh

--- Script4.sh
+++ Script4.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Frias,Lucas
+#Canteros,Luciano
+#Fecha: 17/10/2019
+
+#Descripcion: Scrip que busca archivos con determinada extension, dentro de un directorio determinado de manera recursiva.
 	
 
 	read -p 'Ingrese un directorio a buscar : ' directorio
@@ -13,8 +18,8 @@
 		cd $directorio
 		
 		
-		find . -name $extension >> archivos-$extension-en-$directorio
-		
+		find . -name $extension >> archivos-$extension-en-$directorio #find -name busca archivos que coincidan con el name ingresado
+										# y los escribe en un archivo
 
 
 
